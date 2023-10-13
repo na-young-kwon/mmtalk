4e73dea1bb56aa075a1f3ad26a845d2266ee6fc5
na-young-kwon <kny0651@naver.com>
2023-10-14 02:27:34 +0900

    프로젝트 생성

---
+++ mmtalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mmtalk
+//
+//  Created by 권나영 on 2023/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
